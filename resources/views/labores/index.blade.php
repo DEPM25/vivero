567be9804e1cf57bc7ca605383f0eca679b266ab
DEPM25 <zeroxz78@gmail.com>
2024-11-18 22:55:12 -0500

    se agrego el estilo de tailwind

--- resources/views/labores/index.blade.php
+++ resources/views/labores/index.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-<div class="py-12">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
